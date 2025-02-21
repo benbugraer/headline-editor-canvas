acfdc67cb4c530064d1526f1c7c6dd288b51eb85
benbugraer <bugraerdev@gmail.com>
2025-02-21 19:36:39 +0300

    refactor: Simplify package configuration and exports
    - Modify index.ts to use default export for HeadlineEditorCanvas
    - Streamline package.json by removing complex export configuration
    - Update tsconfig.json to use CommonJS module and simplify compiler options
    - Remove Next.js and build tool dependencies
    - Bump version to 2.0.0 to reflect significant configuration changes

--- index.ts
+++ index.ts
@@ -1,6 +1,9 @@
 "use client";
 
-export { default as HeadlineEditorCanvas } from "./components/Canvas/Canvas";
+import HeadlineEditorCanvas from "./components/Canvas/Canvas";
+
+export default HeadlineEditorCanvas;
+
 export * from "./components/Canvas/types";
 export * from "./components/Canvas/utils";
 export * from "./components/Canvas/hooks";
